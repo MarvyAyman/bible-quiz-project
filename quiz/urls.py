59691641490105891038a579583714cddff40143
MarvyAyman <marvi.ayman900211@ci.menofia.edu.eg>
2026-06-15 20:58:03 +0300

    Update regex patterns for quiz_slug to support Arabic

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,16 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
-    re_path(r'^quiz/(?P<quiz_slug>[\w-]+)/$', views.quiz_view, name='quiz_view'),
-    re_path(r'^quiz/(?P<quiz_slug>[\w-]+)/submit/$', views.submit_score, name='submit_score'),
-    re_path(r'^quiz/(?P<quiz_slug>[\w-]+)/leaderboard/$', views.leaderboard_view, name='leaderboard_view'),
+    re_path(r'^quiz/(?P<quiz_slug>[\w\u0600-\u06FF%-]+)/$', views.quiz_view, name='quiz_view'),
+    re_path(r'^quiz/(?P<quiz_slug>[\w\u0600-\u06FF%-]+)/submit/$', views.submit_score, name='submit_score'),
+    re_path(r'^quiz/(?P<quiz_slug>[\w\u0600-\u06FF%-]+)/leaderboard/$', views.leaderboard_view, name='leaderboard_view'),
 
     # Panel (staff-only)
     path('panel/', views.panel_dashboard, name='panel_dashboard'),
     path('panel/quizzes/', views.manage_quizzes, name='manage_quizzes'),
-    re_path(r'^panel/quiz/(?P<quiz_slug>[\w-]+)/edit/$', views.edit_existing_quiz, name='edit_existing_quiz'),
-    re_path(r'^panel/quiz/(?P<quiz_slug>[\w-]+)/delete/$', views.delete_quiz, name='delete_quiz'),
+    re_path(r'^panel/quiz/(?P<quiz_slug>[\w\u0600-\u06FF%-]+)/edit/$', views.edit_existing_quiz, name='edit_existing_quiz'),
+    re_path(r'^panel/quiz/(?P<quiz_slug>[\w\u0600-\u06FF%-]+)/delete/$', views.delete_quiz, name='delete_quiz'),
     path('panel/quiz/new/', views.create_quiz_step1, name='create_quiz_step1'),
     path('panel/quiz/preview/', views.create_quiz_preview, name='create_quiz_preview'),
-    re_path(r'^panel/quiz/(?P<quiz_slug>[\w-]+)/publish/$', views.create_quiz_publish, name='create_quiz_publish'),
+    re_path(r'^panel/quiz/(?P<quiz_slug>[\w\u0600-\u06FF%-]+)/publish/$', views.create_quiz_publish, name='create_quiz_publish'),
 ]
